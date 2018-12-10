840d7756c4d7e0e7de1ab5b9f6421389f13990c3
casonhawley <cason.hawley@gmail.com>
2018-12-10 16:15:55 -0700

    refactored quite a bit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,27 +3,29 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
-import Signup from './components/signup';
-import Signin from './components/signin';
-import HeaderWrapper from './components/headerWrapper';
+import Layout from './components/layout';
+
+import Signup from './components/auth/signup';
+import Signin from './components/auth/signin';
+
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <HeaderWrapper>
-            <Route path='/' component={Signin}/>
-            <Route path='/' component={Signup}/>
-          </HeaderWrapper>
+          <Layout>
+            <Route path='/' exact component={Signin}/>
+            <Route path='/signin' component={Signin}/>
+            <Route path='/signup' component={Signup}/>
+          </Layout>
         </Switch>
       </BrowserRouter>
     </Provider>
