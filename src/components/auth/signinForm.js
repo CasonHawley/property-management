42043e626d3bb293ced643cad2db38e708fc9ee6
casonhawley <cason.hawley@gmail.com>
2018-12-12 15:43:25 -0700

    login Field component, text-link component, and remaining signin styles

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,33 +1,47 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
- import { FormTitle } from "../formTitle";
-import { FormInput } from "../formFields";
- class SigninForm extends Component {
-    render() {
-        return (
-          <form className="sign-in-form">
-            <FormTitle className="sign-in-form__title" text="Login" />
-            <Field
-              className="sign-in-form__email"
-              placeholder="Enter Email"
-              name="email"
-              type="email"
-              title="Email"
-              component={FormInput}
-            />
-            <Field
-              className="sign-in-form__password"
-              placeholder="Enter Password"
-              name="password"
-              type="password"
-              title="Password"
-              component={FormInput}
-            />
-          </form>
-        );
-      }
+
+import { FormTitle } from "../formTitle";
+import { FormInput, FormButton } from "../formFields";
+import TextLink from "../textLink";
+
+class SigninForm extends Component {
+  render() {
+    return (
+      <form className="sign-in-form">
+        <FormTitle className="sign-in-form__title" text="Login" />
+        <Field
+          className="sign-in-form__email"
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          title="Email"
+          component={FormInput}
+        />
+        <Field
+          className="sign-in-form__password"
+          placeholder="Enter Password"
+          name="password"
+          type="password"
+          title="Password"
+          component={FormInput}
+        />
+        <Field
+          className="sign-in-form__login"
+          name="login"
+          type="submit"
+          title="Login"
+          component={FormButton}
+        />
+        <TextLink to='/forgot' text='Forgot Password' />
+        <TextLink to='/signup' text='Not a member? Register here' />
+      </form>
+    );
+  }
 }
- SigninForm = reduxForm({
-    form: "signin"
+
+SigninForm = reduxForm({
+  form: "signin"
 })(SigninForm);
- export default SigninForm;
+
+export default SigninForm;
